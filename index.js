c3c79b5061361994f836224be4706800dc87b07f
Logan Negley <logannegley@gmail.com>
2020-04-15 18:22:18 -0500

    index.js set up with proper router information for both post and users routers

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
 server.use(express.json());
+server.use('/api/user');
+server.use('/api/posts');
 
 
 server.listent(4000, ()=>{
