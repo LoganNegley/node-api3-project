a610e5017ebba61783b117ea5f18063ad800cd9b
Logan Negley <logannegley@gmail.com>
2020-04-15 18:03:10 -0500

    imported in index.js and started server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code away!
+const express = require('express');
+const userRouter = require('./users/userRouter');
+
+const server = express();
+server.use(express.json());
+
+
+server.listent(4000, ()=>{
+    console.log('Listening on port 4000')
+});
