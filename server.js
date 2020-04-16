0a0bbf4256323e3ddf8a06debe8e063399c91d6c
Logan Negley <logannegley@gmail.com>
2020-04-16 10:12:27 -0500

    nameInsert added to get / on server

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ server.use('/api/posts', postRouter);
 
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  const nameInsert = (req.body.name ? `${req.body.name}` : '');
+  res.send(`<h2>Let's write some middleware ${nameInsert}!</h2>`);
 });
 
 //custom middleware
